36d88140fc72f784da2f34275fb2ff3e587f2e16
bbaserdem <baserdemb@gmail.com>
2025-02-03 22:08:49 -0600

    Added filetype plugins

---
+++ ftplugin/lua.lua
@@ -0,0 +1,8 @@
+-------------------------------------------------
+-- Lua files behavior
+-------------------------------------------------
+-- Custom tablength; 2
+-- Using vim.bo so that these are only established for the buffer
+vim.bo.shiftwidth = 2       -- size of an indentation (sw).
+vim.bo.tabstop = 2          -- size of a hard tabstop (ts).
+vim.bo.softtabstop = 2      -- number of spaces a <Tab> counts for.
