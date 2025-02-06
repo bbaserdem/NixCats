72e0e2dc8c3cd9a03a67f4e656e25ed8ae9833d2
bbaserdem <baserdemb@gmail.com>
2025-02-06 13:10:13 -0600

    Trying to fix blink-cmp
    Trying to fix stuff

--- lua/pluginConf/completion/init.lua
+++ lua/pluginConf/completion/init.lua
@@ -5,4 +5,25 @@
 require('lze').load {
     -- Category things
     { import = 'pluginConf.completion.blink', },
+    -- Shared completion sources
+    {
+        'cmp-vimtex',
+        on_plugin = {
+            'blink.cmp',
+            'nvim-cmp',
+        },
+    },
+    -- Luasnip config
+    {
+        'friendly-snippets',
+        for_cat = 'completion.luasnip',
+        on_plugin = { 'LuaSnip', },
+    }, {
+        'LuaSnip',
+        for_cat = 'completion.luasnip',
+        on_plugin = {
+            'blink.cmp',
+            'nvim-cmp',
+        },
+    }
 }
