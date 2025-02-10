05f7f2adbad768830e04b4f2c86f22f4e0de136e
bbaserdem <baserdemb@gmail.com>
2025-02-10 13:26:46 -0600

    Snacks
    Configing snacks

--- lua/pluginConf/startup/snacks/dashboard.lua
+++ lua/pluginConf/startup/snacks/dashboard.lua
@@ -4,6 +4,23 @@
 -- Configuring dashboard
 return {
   enabled = true,
+  -- We don't want lazy as a function
+  keys = {
+    { icon = " ", key = "f", desc = "Find File",
+      action = ":lua Snacks.dashboard.pick('files')" },
+    { icon = " ", key = "n", desc = "New File",
+      action = ":ene | startinsert" },
+    { icon = " ", key = "g", desc = "Find Text",
+      action = ":lua Snacks.dashboard.pick('live_grep')" },
+    { icon = " ", key = "r", desc = "Recent Files",
+      action = ":lua Snacks.dashboard.pick('oldfiles')" },
+    { icon = " ", key = "c", desc = "Config",
+      action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+    { icon = " ", key = "s", desc = "Restore Session",
+      section = "session" },
+    { icon = " ", key = "q", desc = "Quit",
+      action = ":qa" },
+  },
   -- Using the advanced template
   sections = {
     { section = "header" },
