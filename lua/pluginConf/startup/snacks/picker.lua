d3d1773c7a09f9677c9e5d750a605acb43f11027
bbaserdem <baserdemb@gmail.com>
2025-02-10 23:03:01 -0600

    Snacks picker
    Explorer not in nixpkgs (unstable) yet

--- lua/pluginConf/startup/snacks/picker.lua
+++ lua/pluginConf/startup/snacks/picker.lua
@@ -42,54 +42,8 @@ return {
       diagnostics_open = false,
       watch = true,
       -- Other opts
-      finder = 'explorer',
-      sort = { fields = { 'sort' } },
       supports_live = true,
-      focus = 'list',
-      auto_close = false,
-      jump = { close = false },
-      layout = { preset = "sidebar", preview = false },
-      formatters = {
-        file = { filename_only = true },
-        severity = { pos = "right" },
-      },
       matcher = { sort_empty = false, fuzzy = true, },
-      config = function(opts)
-        return require('snacks.picker.source.explorer').setup(opts)
-      end,
-      win = {
-        list = {
-          keys = {
-            ['<BS>'] = 'explorer_up',
-            ['l'] = 'confirm',
-            ['h'] = 'explorer_close', -- close directory
-            ['a'] = 'explorer_add',
-            ['d'] = 'explorer_del',
-            ['r'] = 'explorer_rename',
-            ['c'] = 'explorer_copy',
-            ['m'] = 'explorer_move',
-            ['o'] = 'explorer_open', -- open with system application
-            ['P'] = 'toggle_preview',
-            ['y'] = 'explorer_yank',
-            ['u'] = 'explorer_update',
-            ['<c-c>'] = 'tcd',
-            ['<leader>/'] = 'picker_grep',
-            ['<c-t>'] = 'terminal',
-            ['.'] = 'explorer_focus',
-            ['I'] = 'toggle_ignored',
-            ['H'] = 'toggle_hidden',
-            ['Z'] = 'explorer_close_all',
-            [']g'] = 'explorer_git_next',
-            ['[g'] = 'explorer_git_prev',
-            [']d'] = 'explorer_diagnostic_next',
-            ['[d'] = 'explorer_diagnostic_prev',
-            [']w'] = 'explorer_warn_next',
-            ['[w'] = 'explorer_warn_prev',
-            [']e'] = 'explorer_error_next',
-            ['[e'] = 'explorer_error_prev',
-          },
-        },
-      },
     },
   },
 }
