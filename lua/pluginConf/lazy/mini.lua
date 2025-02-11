5eecd0fe9383f50d11471f3bae428fe334c4e189
bbaserdem <baserdemb@gmail.com>
2025-02-10 19:55:19 -0600

    Mini
    Configing mini

--- lua/pluginConf/lazy/mini.lua
+++ lua/pluginConf/lazy/mini.lua
@@ -49,13 +49,50 @@ return {
     -- Move with square brackets
     require('mini.bracketed').setup()
     -- Which key
-    require('mini.clue').setup()
+    local miniclue = require('mini.clue')
+    miniclue.setup({
+      triggers = {
+        -- Leader triggers
+        { mode = 'n', keys = '<Leader>' },
+        { mode = 'x', keys = '<Leader>' },
+        -- Built-in completion
+        { mode = 'i', keys = '<C-x>' },
+        -- `g` key
+        { mode = 'n', keys = 'g' },
+        { mode = 'x', keys = 'g' },
+        -- Marks
+        { mode = 'n', keys = "'" },
+        { mode = 'n', keys = '`' },
+        { mode = 'x', keys = "'" },
+        { mode = 'x', keys = '`' },
+        -- Registers
+        { mode = 'n', keys = '"' },
+        { mode = 'x', keys = '"' },
+        { mode = 'i', keys = '<C-r>' },
+        { mode = 'c', keys = '<C-r>' },
+        -- Window commands
+        { mode = 'n', keys = '<C-w>' },
+        -- `z` key
+        { mode = 'n', keys = 'z' },
+        { mode = 'x', keys = 'z' },
+      },
+      clues = {
+        -- Enhance this by adding descriptions for <Leader> mapping groups
+        miniclue.gen_clues.builtin_completion(),
+        miniclue.gen_clues.g(),
+        miniclue.gen_clues.marks(),
+        miniclue.gen_clues.registers(),
+        miniclue.gen_clues.windows(),
+        miniclue.gen_clues.z(),
+      },
+    })
     -- Jump to character
     require('mini.jump').setup()
     -- Highlight word under cursor
     require('mini.cursorword').setup()
     -- Highlight certain stuff
-    require('mini.hipatterns').setup({
+    local minihp = require('mini.hipatterns')
+    minihp.setup({
       highlighters = {
         -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
         fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
@@ -63,7 +100,7 @@ return {
         todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
         note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
         -- Highlight hex color strings (`#rrggbb`) using that color
-        hex_color = require('mini.hipatterns').gen_highlighter.hex_color(),
+        hex_color = minihp.gen_highlighter.hex_color(),
       },
     })
     -- Icons for file types
