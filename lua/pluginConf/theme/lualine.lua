64d9b06b4e6c6a9655f6444704819ba1dac7e7a9
bbaserdem <baserdemb@gmail.com>
2025-02-16 11:54:40 -0600

    Colortheme fixes

--- lua/pluginConf/theme/lualine.lua
+++ lua/pluginConf/theme/lualine.lua
@@ -47,7 +47,7 @@ return{
     require('lualine').setup({
       options = {
         icons_enabled = true,
-        theme = 'horizon',
+        theme = 'auto',
         component_separators =  { left = '', right = ''},
         section_separators =    { left = '', right = ''},
         always_divide_middle = true,
