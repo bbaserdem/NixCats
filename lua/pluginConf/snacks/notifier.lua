8b1c15eea04f5c31b0bbc87c395d55710f6c47e6
bbaserdem <baserdemb@gmail.com>
2025-02-16 14:33:17 -0600

    Added nil as an lsp

--- lua/pluginConf/snacks/notifier.lua
+++ lua/pluginConf/snacks/notifier.lua
@@ -1,21 +1,23 @@
 -- <nixCats>/lua/pluginConf/snacks/notifier.lua
--- Notification daemon 
+-- Notification daemon
 
 return {
-  enabled = true,
-  icons = {
-    error = " ",
-    warn =  " ",
-    info =  " ",
-    debug = " ",
-    trace = " ",
-  },
-  keep = function(notif)
-    -- Keep notifications on screen if in commandline
-    return vim.fn.getcmdpos() > 0
-  end,
-  style = 'fancy',
-  top_down = true,
-  date_format = '%R',
-  more_format = ' ↓ %d lines ',
+	enabled = true,
+	margin = {
+		top = 1,
+		right = 1,
+		bottom = 1,
+	},
+	icons = {
+		error = " ",
+		warn = " ",
+		info = " ",
+		debug = " ",
+		trace = " ",
+	},
+	keep = false,
+	style = "fancy",
+	top_down = true,
+	date_format = "%R",
+	more_format = " ↓ %d lines ",
 }
