ffcf0524bb3ed8438f26395085252401eb85b098
bbaserdem <baserdemb@gmail.com>
2025-02-26 09:34:02 -0600

    Fixing keybinds

--- lua/keymapConf/symbols.lua
+++ lua/keymapConf/symbols.lua
@@ -5,7 +5,7 @@
 vim.keymap.set("n", "<leader>so", "<cmd>AerialToggle<CR>", { desc = "Aerial menu" })
 vim.keymap.set("n", "<leader>sO", "<cmd>AerialToggle!<CR>", { desc = "Aerial menu (no focus)" })
 vim.keymap.set("n", "<leader>sn", "<cmd>AerialNavToggle<CR>", { desc = "Aerial navigation" })
-vim.keymap.set("n", "<leader>sN", require("aerial").snacks_picker, { desc = "Aerial navigation (snacks)" })
+--vim.keymap.set("n", "<leader>sN", require("aerial").snacks_picker, { desc = "Aerial navigation (snacks)" })
 
 -- Neotree
 vim.keymap.set("n", "<leader>sf", "<cmd>Neotree focus float document_symbols<CR>", { desc = "Neotree symbols" })
