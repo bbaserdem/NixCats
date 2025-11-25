a755def493738b29034a9eb5e9158d22b1503212
bbaserdem <baserdemb@gmail.com>
2025-11-24 21:03:28 -0600

    Flake ~ reorg
    Redoing plugins and organization

--- lua/keymapConf/old/lint.lua
+++
@@ -1,6 +0,0 @@
--- <nixCats>/lua/keymapConf/lint.lua
--- <Leader>l: Linting and styling
-local conform_keymap = require("lzextras").keymap("conform.nvim")
-
--- Symbol menu by aerial
-conform_keymap.set({ "n", "v" }, "<Leader>lf", require("conform").format, { desc = "Formatter (conform)" })
--- lua/pluginConf/old/snacks/toggle.lua
+++
@@ -1,8 +0,0 @@
--- <nixCats>/lua/pluginConf/snacks/toggle.lua
--- Configuration of toggle things
-
-return {
-  enabled = true,
-  map = require("lzextras").keymap("snacks.nvim").set,
-  notify = true,
-}
