b38611047453480d909914f10b6c087cac2e8a51
bbaserdem <baserdemb@gmail.com>
2025-12-09 13:03:31 -0600

    Keyboard shortcuts
    Added few keyboard shortcuts

--- lua/keymapConf/init.lua
+++ lua/keymapConf/init.lua
@@ -28,3 +28,26 @@ if wk_status then
     { "<Leader>v", group = "[V]iew (windows + tabs) functions" },
   })
 end
+
+-- System clipboard functions
+vim.keymap.set({ "n", "v", "x" }, "<C-a>", "gg0vG$", { noremap = true, silent = true, desc = "Select all" })
+vim.keymap.set({ "v", "x", "n" }, "<leader>y", '"+y', { noremap = true, silent = true, desc = "Yank to clipboard" })
+vim.keymap.set(
+  { "n", "v", "x" },
+  "<leader>Y",
+  '"+yy',
+  { noremap = true, silent = true, desc = "Yank line to clipboard" }
+)
+vim.keymap.set({ "n", "v", "x" }, "<leader>p", '"+p', { noremap = true, silent = true, desc = "Paste from clipboard" })
+vim.keymap.set(
+  "x",
+  "<leader>P",
+  '"_dP',
+  { noremap = true, silent = true, desc = "Paste over selection without erasing unnamed register" }
+)
+vim.keymap.set(
+  "i",
+  "<C-p>",
+  "<C-r><C-p>+",
+  { noremap = true, silent = true, desc = "Paste from clipboard from within insert mode" }
+)
